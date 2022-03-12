373fb01512a5a2c9e37210c26c061a00e1df9268
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-12 09:39:27 -0800

    Prints out the sign of a number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+/* more headers goes here */
 
 /* bettystyle doc for function main goes there */
 int main(void)
@@ -8,12 +10,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n < 0)
-		printf("%d is negative/n", n);
+		printf("%d is negative\n", n);
+
 	else if (n == 0)
-		printf("%d is zero/n", n);
+		printf("%d is zero\n", n);
+
 	else
-		printf("%d is postive/n", n);
+		printf("%d is postive\n", n);
+
+
 	return (0);
 }
-
