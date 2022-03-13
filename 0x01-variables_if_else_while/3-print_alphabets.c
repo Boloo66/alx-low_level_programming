d1374e4e6dd56bd1190ddec946a794c672aa860c
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-12 16:12:58 -0800

    Prints all casesof alphabets in English

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* standard input output header file */
+
+/**
+ * int - prints all characterss in lower and upper case
+ *
+ * Description: Prints all cases of letters after the function
+ * Return: returns zero
+ */
+int main(void)
+{
+ 	int ch;
+	
+	for(ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for(ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	printf("\n");
+	
+	return (0);
+}
+		
