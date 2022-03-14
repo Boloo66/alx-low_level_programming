5e27865540fb56fb5b7fc6a94163ce28dc26d60a
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-13 17:47:46 -0700

    Prints alphabets in reverse order using putchar()

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* These are all the header files used */
+
+/**
+ * main- Prints alphabets in reverse order
+ *
+ * Description: All alphabets are printed using putchar() in reverse method
+ * Return: returns zero if code is sucessful
+ */
+int main(void)
+{
+	int ch;
+
+	for(ch = 'z'; ch <= 'a'; ch--)
+	{
+	putchar(ch);
+	}
+
+	printf("\n");
+	return (0);
+}
