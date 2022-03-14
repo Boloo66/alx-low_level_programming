2c330f734f1ecc63c0b3b47c934e4cec2d0f8259
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-14 16:44:58 -0700

    Prints decimal numbers using putchar function

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,17 +6,18 @@
  * main - prints numbers using putchar
  *
  * Description: prints numbers using only putchar function
- * return: returns zero when programme is completed
+ * Return: returns zero when programme is completed
  */
 int main(void)
 {
 	int ch;
 
 	ch = 'a';
-	while (ch <= 9)
+	while (ch <= '9')
 	{
 		putchar(ch);
 		ch++;
 	}
+	putchar("\n");
 	return (0);
 }
