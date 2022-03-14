b3cf2b936ec5da8964b3f4ae407789f8b05de6df
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-13 20:44:49 -0700

    Prints the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,9 +4,11 @@
 /* These are the headers used */
 
 /**
- * main- This function prints the last number of a randomly generated integer
+ * main- This function prints the last number
+ * of a randomly generated integer
  *
- * Description: This function simply gets the last digit of a number by using the Mod operator
+ * Description: This function simply gets the last 
+ * digit of a number by using the Mod operator
  * return: This returns zero when the program has finished
  */
 int main(void)
@@ -15,21 +17,22 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	ldigit = n%10;
+	ldigit = n % 10;
 
 	if (ldigit == 0)
 		{
-			printf("Last digit of %d is %d and is %d", n, ldigit,0);
+		printf("Last digit of %d is %d and is %d", n, ldigit, 0);
 			
 		}
 	else if (ldigit >= 5)
 		{
-		
-			printf("Last digit of %d is %d and is greater than %d", n, ldigit, 5);
+		printf("Last digit of %d is %d and is greater\
+			       	than %d", n, ldigit, 5);
 		}
 	else if (ldigit > 0 && ldigit < 5)
 		{
-			printf("Last digit of %d is %d and is less than %d and not %d", n, ldigit, 6, 0);
+		printf("Last digit of %d is %d and is less than\
+			       	%d and not %d", n, ldigit, 6, 0);
 		}
 	printf("\n");
 	return (0);
