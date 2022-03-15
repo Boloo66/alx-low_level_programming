ca2f5d2e6c435749267eec48618655165f904fa7
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-14 17:20:33 -0700

    Print hex numbers and alphabets

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Headers used for the project */
+
+/*
+ * main- Prints hex numbers
+ *
+ * Description: This prints all the hex digits and alphabets
+ * Return: returns zero after final execution
+ */
+int main(void)
+{
+	int ch;
+	int i;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar (i);
+	}
+	putchar('\n');
+	return (0);
+}
