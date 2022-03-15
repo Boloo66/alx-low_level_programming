32454f5f7eedd3255e81e833540620f3076550a1
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-14 18:28:39 -0700

    Prints characters and numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Header files for this project */
+
+/**
+ * main- prints characters
+ *
+ * Description: This just prints
+ * Return: returns characters and numbers
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 'i', i <= '8', i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('9');
+	putchar('\$');
+	return (0);
+}
