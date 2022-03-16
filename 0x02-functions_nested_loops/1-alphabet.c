5c29b1f4b297f86844d7202396c85c52009bf635
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-16 20:21:47 +0000

    Prints alphabets a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* This is for the main header file */
+
+/**
+ * print_alphabet- prints alphabets from a-z
+ *
+ * Return: Always 0
+ * }
+ */
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
