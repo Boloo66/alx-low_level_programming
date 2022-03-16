108037ffae6413925d8a015396406085a2b1b191
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-16 20:19:01 +0000

    Prints alphabets from a-z ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/* main header file */
+
+/**
+ * print_alphabet_x10 - print alphabets 10x
+ *
+ * Return - Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int count;
+
+	count = 0;
+	while (count < 10);
+	{
+	for (i = 'a'; i <= 'z'; i++)
+	{ 
+		_putchar(i);
+	}
+	count++;
+	_putchar('\n');
+	}
+	return (0);
+}
+				
+
+	
