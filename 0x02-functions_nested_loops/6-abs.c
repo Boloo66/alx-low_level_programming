9c61d1cf4897ede59c4eebf561f03fc079938dfb
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-16 21:18:25 +0000

    returns absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/* main header file here */
+
+/**
+ * _abs - finds the absolute vale of a number
+ *
+ * @n: holds the number
+ * Return: returns the absolute value of the number n
+ */
+int _abs(int n);
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n);
+	}
+}
