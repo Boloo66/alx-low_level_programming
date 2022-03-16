1b930bdfbd7d6353e0ae19a0ea0e8789c53825c6
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-16 19:49:43 +0000

    This prints putchar to the stdout

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* This is the main header file */
+
+/**
+ * main - Entry point
+ *
+ * Return: returns zero when execution is successful
+ */
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
+	return (0);
+}
