192cfb95b28a72e0f5aab77c4da1403ed9a94ed8
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-16 23:55:18 +0000

    Adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* Main header file */
+
+/**
+ * add - adds two numbers together
+ *
+ * @i: holds one value
+ * @k: holds another value
+ * Return: returns zero
+ */
+int add(int i, int k)
+{
+	return(i + k);
+}
