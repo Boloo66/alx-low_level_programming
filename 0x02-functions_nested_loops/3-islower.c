f4e3b2b85eeee5288941bcc0bd5c307366d26c90
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-16 20:43:36 +0000

    Checks  if an alphabet is in lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* Main header files here */
+
+/**
+ * _islower - checks if an alphabet is in lower case
+ *
+ * Return: return 0 or 1
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <='z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
