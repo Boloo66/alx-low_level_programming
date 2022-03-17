109b17218c12ca826d62d0e6aef10697f09fb3ec
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-17 00:11:07 +0000

    all number grater or less than 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/* main header files*/
+
+/**
+ * print_to_98 - prints from a number till 98
+ *
+ * @n: holds the valur of a number
+ * Return: returns the numbers
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for ( n = n; n < 98; n++)
+		{
+			printf("%d,", n);
+			printf("%d\n", 98);
+		}
+		else
+		{
+			for (n =n; n > 98; n--)
+			{
+				printf("d,", n);
+				printf("%d\n", 98);
+			}
+		}
+	}
+}
