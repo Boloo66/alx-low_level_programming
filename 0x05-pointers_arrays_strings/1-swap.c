7a35c3dc25eebfc99affaecdb8af9efee3292caf
Boloo66 <99683557+Boloo66@users.noreply.github.com>
2022-03-22 21:02:25 +0100

    Update 1-swap.c
    My pointers project

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,7 @@
  *swap_int - swap the values of two integers
  *
  *@a: a pointer to an integer that will be updated
- *b: a pointer to an integer that will also be updated
+ *@b: a pointer to an integer that will also be updated
  *Return: returns void that means our anser is correct
  */
 void swap_int(int *a, int *b)
