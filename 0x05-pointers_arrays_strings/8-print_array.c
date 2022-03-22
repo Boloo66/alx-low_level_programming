28c6f4306e69fc218a29de48b19618fb95cd46b2
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-22 04:43:38 +0000

    My pointer project

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements
+ *
+ * @a: A pointer
+ * @n: return value n
+ * Return: void which means correct
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d".\, a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf('\n');
+}
