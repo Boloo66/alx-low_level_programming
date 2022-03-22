b11c046a119a7eef23416636ddaf0b0d0acbcf22
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-22 04:34:05 +0000

    My pointers project

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * 
+ * @s: A POINTER to an int that will be updated
+ * Return - void means correct
+ */
+int _strlen(char *s)
+{
+	int u;
+
+	u = 0;
+	while (s[u] != '\0')
+	{
+		u++;
+	}
+	return (u);
+}
