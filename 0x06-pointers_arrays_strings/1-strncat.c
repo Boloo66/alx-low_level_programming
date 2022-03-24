b81de9fc62a465ccc569be98a02643710aae7ac2
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-24 05:34:00 +0000

    Concanates strings to n bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
+ * _strncat - concatenates strings of n bytes
+ * @dest: string 1
+ * @src: string 2
+ * @n: number of bytes for string src
  */
 char *_strncat(char *dest, char *src, int n)
+
 {
-	char *_strncat(char *dest, char *src, int n)
+	int i, j;
 
-	{
-		int i, j;
-
-		i = 0;
-		while (dest[i] != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-		j = 0;
-		while (j < n && src[j] != '\0')
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
