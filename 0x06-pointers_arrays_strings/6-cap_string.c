6ffa39b60cd798d9607b72383fefad5b1a46fd27
Boloo66 <99683557+Boloo66@users.noreply.github.com>
2022-03-25 22:07:02 +0100

    Update 6-cap_string.c
    Capitalizes strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,6 @@
 char *cap_string(char *s)
 {
 	int count = 0, i;
-	
 	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
@@ -23,9 +22,9 @@ char *cap_string(char *s)
 	{
 			if (*(s + count) == separators[i])
 	{
-		if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-			*(s + (count + 1)) = *(s + (count + 1)) - 32;
-		break;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+				*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 	}
 	}
 		count++;
