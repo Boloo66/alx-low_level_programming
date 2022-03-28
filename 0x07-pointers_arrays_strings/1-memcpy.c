09d251a2f414de15ecff4a4aa0808b0250c413b6
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-03-28 12:22:05 +0000

    My project on even more pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
-nclude "main.h"
-/**
- *  *_memcpy -   copy number bytes form adress "from" to adress "to"
+include "main.h"
+
+/**_memcpy -   copy number bytes form adress "from" to adress "to"
  *   *@dest: content is to be copied
  *    *@src: Source of data to be copied
  *     *@n: bytes of the memory
  *      *Return: dest
- *       */
+ *      */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 
