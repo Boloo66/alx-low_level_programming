fb66ebe81b2a2400f79845f75f7a38993eab0a16
Boloo66 <99683557+Boloo66@users.noreply.github.com>
2022-04-02 05:01:38 +0100

    Update 1-args.c
    argc and argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
- *  * main - print the name of the program
- *   * @argc: Count arguments
- *    * @argv: Arguments
- *     *
- *      * Return: Always 0 (Success)
- *       */
-
-int main(int argc, char *argv[])
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void) argv; /*Ignore argv*/
-	printf("%i\n", argc - 1);
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
