0b87beea863d244e75286f0b36860095d2fb481c
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-04-13 19:50:12 +0100

    My project on pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* print_name - function prints the name on stdout
+ *
+ * @name: pointer to a string
+ * @f: function pointer
+ *
+ * Return: No return value
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if(name || f)
+	{
+		f(name);
+	}
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#def MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+void print_name(char *name, void (*f)(char *));
+#endif
