608cbf4efd779982d9a274b0b639204d3e785144
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-04-14 21:55:55 +0100

    variadic functions in C language

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,18 +9,21 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	va_start(ap, n);
-        unsigned int i, sum = 0;
+    va_list ap;
+    va_start(ap, n);
+    int sum = 0;
 
-	if(n == 0)
-	{
-		return(0);
-	}
-	for(i = 1; i <= n; i++)
-	{
-		sum += va_args(ap, int);
-	}
-	va_end(ap);
-	return(sum);
+    unsigned int i;
+
+    if (n == 0)
+    {
+        return (0);
+    }
+    
+    for (i = 1; i <= n; i++)
+    {
+        sum += va_arg(ap, int);
+    }
+    va_end(ap);
+    return (sum);
 }
