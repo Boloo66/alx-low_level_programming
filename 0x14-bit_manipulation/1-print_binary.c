26a43d5b0ee00bd10473f1d356ba8385d5fe0526
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-04-29 23:39:19 +0100

    Bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,33 +9,14 @@
  */
 void print_binary(unsigned long int n)
 {
-	char binary[256];
-	int length = 0;
-
-	do
+	if (n >> 0)
 	{
-		if(n % 2 == 1)
-			binary[length] = '1';
-		else
-			binary[length] = '0';
-		length++;
-		n = n / 2;
-	}while(n != 0);
-	binary[length] = '\0';
-
-	int middle;
-	middle = length / 2;
-	for(int i = 0; i < middle; i++)
-	{
-		char temp = binary[i];
-		binary[i] = binary[length - i - 1];
-		binary[length - 1 - i] = temp;
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-	int link;
-	link = 0;
-	while(binary[link] != '\0')
+	else
 	{
-		_putchar(binary[link++]);
+		_putchar('0');
 	}
-
 }
