5481dbd5ef586ad2f968bff1296c8895f5df6067
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-06-13 22:26:11 +0100

    dynamic library and linking

---
+++ 0x18-dynamic_libraries/memcpy.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    char name;
+    name = 'A';
+    char *temp = malloc(sizeof(name));
+    return temp;
+}
---
+++ 0x18-dynamic_libraries/strstr.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+    char name;
+    name = 'A';
+    char *temp = malloc(sizeof(name));
+    return temp;
+}
