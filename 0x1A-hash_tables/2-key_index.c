54ea799cf8de5a6a4c46a1bd109f6f455c726675
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-07-01 23:31:26 +0100

    hash tables in c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index- the function returns an index for a hash fuction key
+ *
+ * @key: pointer to a char
+ * @size: size of the array
+ *
+ * Return: returns the index generated
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+{
+	return (hash_djb2(key) % size);
+}
