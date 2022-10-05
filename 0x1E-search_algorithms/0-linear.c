b14c93870d4a2dc693cd082ed3d441374f881230
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-10-05 23:08:57 +0100

    search algo in c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+/**
+* linear_search- searches a list using linear search algo
+* @array: The array to search.
+* @size: The size of the given array.
+* @value: The data to search the array for.
+* Return: returns the index of the value if found
+* or -1 for no search results.
+*/
+
+int linear_search(int *array, size_t size, int value){
+
+    int x= (int)size;
+    if(array == NULL)
+        return (-1);
+        
+    for(x = 0; x < (int)size; x++){
+        printf("Value checked array[%d] = [%d]\n", x, array[x]);
+        if(value == array[x])
+            return x;            
+    }
+    return -1;
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,6 @@
+#ifndef _ALGO_
+#define _ALGO_
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+#endif /*_ALGO_*/
